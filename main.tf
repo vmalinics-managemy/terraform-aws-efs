53cb7d9366ef30cb8428f672884959b845b86385
vmalinics <vitalijs.malinics@managemy.com>
2020-04-09 16:46:25 +0100

    syntax fix

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_efs_mount_target" "default" {
   file_system_id  = join("", aws_efs_file_system.default.*.id)
   ip_address      = var.mount_target_ip_address
   subnet_id       = var.subnets[count.index]
-  security_groups = [length(var.security_groups) > 0 ? var.security_groups : join("", aws_security_group.efs.*.id)]
+  security_groups = length(var.security_groups) > 0 ? var.security_groups : [join("", aws_security_group.efs.*.id)]
 }
 
 resource "aws_security_group" "efs" {
