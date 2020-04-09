140ae7b5bfbf958b990dddc927e1bd37e3b94859
vmalinics <vitalijs.malinics@managemy.com>
2020-04-09 12:27:48 +0100

    updated module url

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  #source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=vm"
   enabled     = var.enabled
   namespace   = var.namespace
   name        = var.name
